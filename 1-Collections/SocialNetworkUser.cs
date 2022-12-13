4d78754115712a074c4627f3aa159fa72d95d35a
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2022-12-13 15:42:26 +0100

    first one

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -6,27 +6,47 @@ namespace Collections
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
+        private readonly IDictionary<String, ISet<TUser>> _followedUsers = new Dictionary<String, ISet<TUser>>(); 
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
         }
 
         public bool AddFollowedUser(string group, TUser user)
         {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
+            if (!_followedUsers.ContainsKey(group))
+            {
+                _followedUsers[group] = new HashSet<TUser>();
+                _followedUsers[group].Add(user);
+            }
+            return _followedUsers[group].Add(user);
         }
 
         public IList<TUser> FollowedUsers
         {
             get
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                var allFriends = new HashSet<TUser>();
+                foreach (String group in _followedUsers.Keys)
+                {
+                    foreach (TUser friend in _followedUsers[group])
+                    {
+                        allFriends.Add(friend);
+                    }
+                }
+                return new List<TUser>(allFriends);
             }
         }
 
         public ICollection<TUser> GetFollowedUsersInGroup(string group)
         {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
+            if (_followedUsers.ContainsKey(group))
+            {
+                return new HashSet<TUser>(_followedUsers[group]);
+            }
+            else
+            {
+                return new HashSet<TUser>();
+            }
         }
     }
 }
